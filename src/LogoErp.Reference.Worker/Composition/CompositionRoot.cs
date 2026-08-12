f4b5a1b32c8232b21c367cc98a417744507ee30b
demiraslanveli <demiraslanveli@gmail.com>
2026-08-12 14:05:23 +0300

    Wire safe Logo session factory into composition root

--- src/LogoErp.Reference.Worker/Composition/CompositionRoot.cs
+++ src/LogoErp.Reference.Worker/Composition/CompositionRoot.cs
@@ -1,4 +1,8 @@
+using System;
+using LogoErp.Reference.Core.Configuration;
+using LogoErp.Reference.Core.Context;
 using LogoErp.Reference.Infrastructure.Configuration;
+using LogoErp.Reference.LogoAdapter.Session;
 
 namespace LogoErp.Reference.Worker.Composition
 {
@@ -8,13 +12,30 @@ namespace LogoErp.Reference.Worker.Composition
 
         public CompositionRoot(LogoErpOptions options)
         {
-            Options = options;
+            Options = options ?? throw new ArgumentNullException(nameof(options));
         }
 
         public static CompositionRoot Build()
         {
             var options = EnvironmentConfigurationLoader.Load();
+            options.Validate();
             return new CompositionRoot(options);
+        }
+
+        public LogoSessionAdapter CreateLogoSession()
+        {
+            var context = new CompanyPeriodContext(
+                Options.FirmNumber,
+                Options.PeriodNumber);
+
+            // Safe default. Replace this bridge only after the exact target
+            // Logo Objects / UnityApplication binding has been verified.
+            ILogoSdkBridge bridge = new UnconfiguredLogoSdkBridge();
+
+            return new LogoSessionAdapter(
+                context,
+                Options,
+                bridge);
         }
     }
 }
