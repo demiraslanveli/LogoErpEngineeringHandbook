0e3e2b65d8abe8369741f235f22235fb73ab123f
demiraslanveli <demiraslanveli@gmail.com>
2026-08-13 08:57:38 +0300

    Add Logo SDK binding key catalog

---
+++ src/LogoErp.Reference.LogoAdapter/Binding/LogoSdkBindingKeys.cs
@@ -0,0 +1,27 @@
+namespace LogoErp.Reference.LogoAdapter.Binding
+{
+    public static class LogoSdkBindingKeys
+    {
+        public const string UnityApplicationType = "SDK.UnityApplication.Type";
+        public const string LoginMethod = "SDK.UnityApplication.LoginMethod";
+        public const string LogoutMethod = "SDK.UnityApplication.LogoutMethod";
+        public const string NewDataObjectMethod = "SDK.UnityApplication.NewDataObjectMethod";
+
+        public const string MaterialDataObjectType = "DataObjectType.Material";
+        public const string CustomerDataObjectType = "DataObjectType.Customer";
+        public const string SalesOrderDataObjectType = "DataObjectType.SalesOrder";
+        public const string DispatchDataObjectType = "DataObjectType.Dispatch";
+        public const string InvoiceDataObjectType = "DataObjectType.Invoice";
+
+        public const string IDataPostMethod = "IData.PostMethod";
+        public const string IDataErrorCode = "IData.ErrorCode";
+        public const string IDataErrorDescription = "IData.ErrorDescription";
+        public const string IDataFields = "IData.DataFields";
+        public const string IDataLines = "IData.Lines";
+
+        public const string ProductionApplicationType = "ProductionApplication.Type";
+        public const string ProductionOpenMethod = "ProductionApplication.OpenMethod";
+        public const string ProductionCloseMethod = "ProductionApplication.CloseMethod";
+        public const string ProductionCreateOrderMethod = "ProductionApplication.CreateProductionOrderMethod";
+    }
+}
